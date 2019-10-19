e03a14cc42edb59582823a1263fc9dcfdf99a9d9
Buhua-Liu <55881942+Buhua-Liu@users.noreply.github.com>
2019-10-19 15:08:53 +0800

    draft

--- main.py
+++ main.py
@@ -1,34 +1,76 @@
 import tensorflow as tf
+import numpy as np
+from dknn import DkNNModel
+from fast_gradient_method import fast_gradient_method
 
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Conv2D, Flatten
+def main():
+    mnist = tf.keras.datasets.mnist
+    (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
 
-mnist = tf.keras.datasets.mnist
-(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+    train_images = train_images.reshape(train_images.shape[0], 28, 28, 1).astype('float32')
+    train_images = (train_images - 127.5) / 127.5
 
-train_images = train_images.reshape(train_images.shape[0], 28, 28, 1).astype('float32')
-train_images = (train_images - 127.5) / 127.5
+    test_images = test_images.reshape(test_images.shape[0], 28, 28, 1).astype('float32')
+    test_images = (test_images - 127.5) / 127.5
 
-test_images = test_images.reshape(test_images.shape[0], 28, 28, 1).astype('float32')
-test_images = (test_images - 127.5) / 127.5
+    #use a holdout of the test data to simulate calibration data for the DkNN
+    nb_cali = 750
+    train_data = train_images
+    cali_data = test_images[:nb_cali]
+    y_cali = test_labels[:nb_cali]
+    test_data = test_images[nb_cali:]
+    y_test = test_labels[nb_cali:]
 
-nb_filters = 64
-nb_classes = 10
+    #Define callable that returns a dictionary of all activations for a dataset
+    def get_activations(data):
+        data_activations = {}
+        for layer in layers:
+            output = model.get_layer(layer).output
+            extractor = tf.keras.Model(model.input,output)
+            print(output.shape)
+            data_activations[layer] = extractor(data)
+        return data_activations
 
-model = Sequential([
-    Conv2D(nb_filters, 8, 2, "same", activation='relu', input_shape= (28, 28, 1)),
-    Conv2D(nb_filters * 2, 6, 2, "valid", activation='relu'),
-    Conv2D(nb_filters * 2, 5, 1, "valid", activation='relu'),
-    Flatten(),
-    Dense(nb_classes, activation='softmax')
-])
+    #load the trained model
+    model = tf.keras.models.load_model('mnist.h5')
 
-model.summary()
+    # Extract representations for the training and calibration data at each layer of interest to the DkNN.
+    layers = ['conv2d', 'conv2d_1', 'conv2d_2', 'dense']
 
-model.compile(optimizer='adam',
-              loss='sparse_categorical_crossentropy',
-              metrics=['accuracy'])
 
-model.fit(train_images, train_labels, batch_size=500, epochs=6)
+    neighbors = 75
+    nb_classes = 10
+    number_bits =17
 
-model.evaluate(test_images, test_labels)
+    dknn_model = DkNNModel(
+            neighbors,
+            layers,
+            get_activations,
+            train_data,
+            train_labels,
+            nb_classes,
+            number_bits = number_bits
+    )
+
+    dknn_model.calibrate(cali_data, cali_labels)
+
+    #Generate adversarial examples
+    adv = fast_gradient_method(
+            model,
+            test_data,
+            eps = 0.25,
+            norm = np.inf
+            clip_min = -1.,
+            clip_max = 1.,
+    )
+
+    #Test the DkNN on clean test data and FGSM test data
+    for data_in, fname in zip([test_data, adv], ['test', 'adv'):
+        dknn_preds = dknn.fprop_np(data_in)
+        print(dknn_preds.shape)
+        print(np.mean(np.argmax(dknn_preds, axis=1) == np.argmax(y_test, axis=1)))
+        DkNNModel.plot_reliability_diagram(dknn_preds, np.argmax(
+                y_test, axis=1), '/tmp/dknn_' + fname + '.pdf')
+
+if __name__ == '__main__':
+    main()
---
+++ training.py
@@ -0,0 +1,35 @@
+import tensorflow as tf
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Conv2D, Flatten
+
+mnist = tf.keras.datasets.mnist
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+
+train_images = train_images.reshape(train_images.shape[0], 28, 28, 1).astype('float32')
+train_images = (train_images - 127.5) / 127.5
+
+test_images = test_images.reshape(test_images.shape[0], 28, 28, 1).astype('float32')
+test_images = (test_images - 127.5) / 127.5
+
+nb_filters = 64
+nb_classes = 10
+
+model = Sequential([
+    Conv2D(nb_filters, 8, 2, "same", activation='relu', name = 'conv2d', input_shape= (28, 28, 1)),
+    Conv2D(nb_filters * 2, 6, 2, "valid", name = 'conv2d_1', activation='relu'),
+    Conv2D(nb_filters * 2, 5, 1, "valid", name = 'conv2d_2', activation='relu'),
+    Flatten(),
+    Dense(nb_classes, name = 'dense', activation='softmax')
+])
+
+model.summary()
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(train_images, train_labels, batch_size=500, epochs=6)
+
+model.evaluate(test_images, test_labels)
+model.save('mnist.h5')
